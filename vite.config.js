f8d76690bf92c29a7b6b114d7889910e335e2c7a
Athena <az2679@nyu.edu>
2023-12-14 19:54:45 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,21 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: resolve(__dirname, 'src'),
   server: {
     port: 3002,
   },
-  root: resolve(__dirname, 'src'),
-  publicDir: resolve(__dirname, 'public'),
+  base: '/github-page/',
   build: {
-    outDir: '../dist',
+    outDir: '../docs',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+      },
+    },
   },
+  // build: {
+  //   outDir: '../dist',
+  // },
+  publicDir: resolve(__dirname, 'public'),
 });
